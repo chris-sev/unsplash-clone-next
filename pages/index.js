3d7c248e4956c410c681edd50f024dff15780713
Chris Sev <chris@better.dev>
2021-03-31 16:55:35 -0700

    fixing!

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,12 @@ export default function Home({ photos }) {
   return (
     <div className="p-20">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-        {photos.map((photo) => (
-          <Image url={photo.file.formats.large.url} key={photo.id} />
-        ))}
+        {photos.map(
+          (photo) =>
+            console.log(photo) || (
+              <Image url={photo.file.formats.large.url} key={photo.id} />
+            )
+        )}
       </div>
     </div>
   );
